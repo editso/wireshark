editso/wireshark

commit d8ac2a046b85823019b6826e505aaa538e223484

Qt: Re-highlight packet bytes when re-selecting packet tree item
Emit the fieldSelected signal when the currently selected dissection
tree item is clicked. This causes the corresponding bytes in the
packet bytes tab to be re-selected.

Change-Id: I9168163f6734ef05ed3196c291a813125d8e86c6
Reviewed-on: https://code.wireshark.org/review/36303
Petri-Dish: Anders Broman <a.broman58@gmail.com>
Tested-by: Petri Dish Buildbot
Reviewed-by: Peter Wu <peter@lekensteyn.nl>
Reviewed-by: Roland Knall <rknall@gmail.com>

ui/qt/proto_tree.cpp
@@ -84,6 +84,8 @@ ProtoTree::ProtoTree(QWidget *parent, epan_dissect_t *edt_fixed) :
 
     connect(this, SIGNAL(expanded(QModelIndex)), this, SLOT(syncExpanded(QModelIndex)));
     connect(this, SIGNAL(collapsed(QModelIndex)), this, SLOT(syncCollapsed(QModelIndex)));
+    connect(this, SIGNAL(clicked(QModelIndex)),
+            this, SLOT(itemClicked(QModelIndex)));
     connect(this, SIGNAL(doubleClicked(QModelIndex)),
             this, SLOT(itemDoubleClicked(QModelIndex)));
 
@@ -582,6 +584,16 @@ void ProtoTree::collapseAll()
     }
     QTreeView::collapseAll();
     updateContentWidth();
+}
+
+void ProtoTree::itemClicked(const QModelIndex &index) {
+    if (index == selectionModel()->selectedIndexes().first()) {
+        FieldInformation finfo(proto_tree_model_->protoNodeFromIndex(index).protoNode());
+
+        if (finfo.isValid()) {
+            emit fieldSelected(&finfo);
+        }
+    }
 }
 
 void ProtoTree::itemDoubleClicked(const QModelIndex &index) {

ui/qt/proto_tree.h
@@ -91,6 +91,7 @@ public slots:
     void collapseSubtrees();
     void expandAll();
     void collapseAll();
+    void itemClicked(const QModelIndex & index);
     void itemDoubleClicked(const QModelIndex & index);
     void selectedFieldChanged(FieldInformation *);
     void selectedFrameChanged(QList<int>);

commit 207cd731bd9fc6e7c5f6dcc8a6e5f2204cc5a6ff

Qt: Fix highlighting of protocol bytes when clicking proto tree item
Commit d8ac2a046b85823019b6826e505aaa538e223484 broke the secondary
highlighting of the surrounding protocol bytes when clicking a proto
tree item, because it didn't find and set the parent field, which
controls that setting.

The field is selected twice by the ByteViewTab when clicked, once
triggered by selectedChanged(), and once by clicked(). Previously
only the former marked the protocol bytes, causing some brief
flickering. Note that keyboard navigation and clicking the ByteViewText
always worked to highlight the protocol bytes. (What is really needed
is an equivalent to selectedChanged() that is sent even when the newly
selected field is the same as the old one.)

Also parent the new FieldInformation with the ProtoTree, so that
ProtoTree::selectedFieldChanged() doesn't process a signal from
the ProtoTree itself and clear and reselect an extra time.

ui/qt/proto_tree.cpp
@@ -652,12 +652,36 @@ void ProtoTree::collapseAll()
 
 void ProtoTree::itemClicked(const QModelIndex &index)
 {
+    // selectionChanged() is not issued when some action would select
+    // the same item as currently selected - but we want to make sure
+    // ByteViewText is highlighting that field. The BVT highlighted bytes
+    // might be different, due to hover highlighting or Find Packet "bytes".
+    //
+    // Unfortunately, clicked() is singled after selectionChanged(), so
+    // we emit fieldSelected() twice after clicking on a new frame, once
+    // in selectionChanged(), and once here.
+    //
+    // We can't get rid of the fieldSelected() handling because there are
+    // non-mouse event ways to select a field, such as keyboard navigation.
+    //
+    // All this would be easier if Qt had a signal similar to
+    // selectionChanged() that was triggered even if the new selection
+    // was the same as the old one.
     if (selectionModel()->selectedIndexes().isEmpty()) {
         emit fieldSelected(0);
     } else if (index == selectionModel()->selectedIndexes().first()) {
-        FieldInformation finfo(proto_tree_model_->protoNodeFromIndex(index));
+        FieldInformation finfo(proto_tree_model_->protoNodeFromIndex(index), this);
 
         if (finfo.isValid()) {
+            // Find and highlight the protocol bytes.
+            QModelIndex parent = index;
+            while (parent.isValid() && parent.parent().isValid()) {
+                parent = parent.parent();
+            }
+            if (parent.isValid()) {
+                FieldInformation parent_finfo(proto_tree_model_->protoNodeFromIndex(parent));
+                finfo.setParentField(parent_finfo.fieldInfo());
+            }
             emit fieldSelected(&finfo);
         }
     }