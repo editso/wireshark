03833462d720254648519ed94cae65d43ac9a4dd
Darius Davis <darius-wireshark@free-range.com.au>
2024-05-22 12:27:41 +1000

    LTE RRC: Use array_length and fix a minor bug.
    If the index exceeded the array length, the comment said it would return the
    last element of the array, but it would actually return the _index of_ the last
    element of the array, which would be ambiguous at best and probably would not
    serve any useful purpose.
    
    It will now return the value of the last element of the array.

--- epan/dissectors/packet-lte-rrc.c
+++ epan/dissectors/packet-lte-rrc.c
@@ -16078,32 +16078,23 @@ static guint32 drx_lookup_onDurationTimer(guint32 idx)
 {
   static const guint32 vals[] = {1,2,3,4,5,6,8,10,20,30,40,50,60,80,100,200};
 
-  if (idx < (sizeof(vals)/sizeof(guint32))) {
-    return vals[idx];
-  }
-  return (sizeof(vals)/(sizeof(guint32)) - 1);
+  return vals[MIN(idx, array_length(vals) - 1)];
 }
 
 static guint32 drx_lookup_inactivityTimer(guint32 idx)
 {
   static const guint32 vals[] = {
-    1,2,3,4,5,6,8,10,20,30,40,50,60,80,100,200,300, 500,750,1280,1920,2560,0
+    1,2,3,4,5,6,8,10,20,30,40,50,60,80,100,200,300,500,750,1280,1920,2560,0
   };
 
-  if (idx < (sizeof(vals)/sizeof(guint32))) {
-    return vals[idx];
-  }
-  return (sizeof(vals)/(sizeof(guint32)) - 1);
+  return vals[MIN(idx, array_length(vals) - 1)];
 }
 
 static guint32 drx_lookup_retransmissionTimer(guint32 idx)
 {
   static const guint32 vals[] = {1,2,4,6,8,16,24,33};
 
-  if (idx < (sizeof(vals)/sizeof(guint32))) {
-    return vals[idx];
-  }
-  return (sizeof(vals)/(sizeof(guint32)) - 1);
+  return vals[MIN(idx, array_length(vals) - 1)];
 }
 
 static guint32 drx_lookup_longCycle(guint32 idx)
@@ -16112,10 +16103,7 @@ static guint32 drx_lookup_longCycle(guint32 idx)
     10,20,32,40,64,80,128,160,256,320,512,640,1024,1280,2048,2560
   };
 
-  if (idx < (sizeof(vals)/sizeof(guint32))) {
-    return vals[idx];
-  }
-  return (sizeof(vals)/(sizeof(guint32)) - 1);
+  return vals[MIN(idx, array_length(vals) - 1)];
 }
 
 static guint32 drx_lookup_longCycle_v1130(guint32 idx)
@@ -16124,10 +16112,7 @@ static guint32 drx_lookup_longCycle_v1130(guint32 idx)
     60,70
   };
 
-  if (idx < (sizeof(vals)/sizeof(guint32))) {
-    return vals[idx];
-  }
-  return (sizeof(vals)/(sizeof(guint32)) - 1);
+  return vals[MIN(idx, array_length(vals) - 1)];
 }
 
 
@@ -16137,10 +16122,7 @@ static guint32 drx_lookup_shortCycle(guint32 idx)
     2,5,8,10,16,20,32,40,64,80,128,160,256,320,512,640
   };
 
-  if (idx < (sizeof(vals)/sizeof(guint32))) {
-    return vals[idx];
-  }
-  return (sizeof(vals)/(sizeof(guint32)) - 1);
+  return vals[MIN(idx, array_length(vals) - 1)];
 }
 
 static void drx_check_config_sane(drx_config_t *config, asn1_ctx_t *actx)
